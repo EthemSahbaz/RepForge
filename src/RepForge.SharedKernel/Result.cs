d32cff6a8ee9b014251834feea02efe202d54b06
Ethem Sahbaz <ethemsahbaz@outlook.com>
2024-11-03 11:08:55 +0100

    Implemented entity base class and result pattern

---
+++ src/RepForge.SharedKernel/Result.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RepForge.SharedKernel;
+public class Result
+{
+    public Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !IsSuccess && error == Error.None)
+        {
+            throw new ArgumentException();
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+
+    }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+    public static Result Success() => new Result(true, Error.None);
+    public static Result Failure(Error error) => new Result(false, error);
+
+    public static Result<TValue> Success<TValue>(TValue value)
+    => new(value, true, Error.None);
+
+    public static Result<TValue> Failure<TValue>(Error error) =>
+        new(default, false, error);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+    public Result(TValue? value,bool isSuccess, Error error) 
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+    [NotNull]
+    public TValue Value => IsSuccess ?
+        _value! : throw new InvalidOperationException("Value of a result can not be accessed.");
+
+    public static implicit operator Result<TValue>(TValue value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue); 
+
+}
