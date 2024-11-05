8771ccd29ff26838c63ded1cf2b6a0a67f1ed49a
Ethem Sahbaz <ethemsahbaz@outlook.com>
2024-11-05 22:22:47 +0100

    Added test project for domain

---
+++ test/RepForge.Domain.UnitTests/Shared/NameTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using RepForge.Domain.Shared;
+
+namespace RepForge.Domain.UnitTests.Shared;
+public class NameTests
+{
+    [Fact]
+    public void Name_With_NullValue_Should_Return_FailureResult()
+    {
+        var name = Name.Create(null);
+
+        name.IsFailure.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Name_With_EmptyString_Should_Return_FailureResult()
+    {
+        var name = Name.Create(string.Empty);
+
+        name.IsFailure.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Name_With_WhiteSpace_Should_Return_FailureResult()
+    {
+        var name = Name.Create("   ");
+
+        name.IsFailure.Should().BeTrue();
+    }
+}
