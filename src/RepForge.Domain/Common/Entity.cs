bedd1e9b3ce0f5d170e3e49c6910d4871832202c
Ethem Sahbaz <ethemsahbaz@outlook.com>
2024-11-07 13:36:26 +0100

    Added common objects

---
+++ src/RepForge.Domain/Common/Entity.cs
@@ -0,0 +1,21 @@
+﻿using RepForge.Domain.Common;
+
+namespace RepForge.SharedKernel;
+public abstract class Entity
+{
+    protected readonly List<IDomainEvent> _domainEvents = new();
+    public Guid Id { get; init; }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public List<IDomainEvent> PopDomainEvents()
+    {
+        var copy = _domainEvents.ToList();
+        _domainEvents.Clear();
+
+        return copy;
+    }
+}
