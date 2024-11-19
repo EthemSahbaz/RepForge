cc49549b8615c86bb9d76fc000ffcf760071b003
Ethem Sahbaz <ethemsahbaz@outlook.com>
2024-11-19 21:10:03 +0100

    Implemented dependency injection and first use case

---
+++ src/RepForge.Infrastructure/Data/InMemory/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using RepForge.Application.Abstractions.Repositories;
+using RepForge.Domain.Users;
+
+namespace RepForge.Infrastructure.Data.InMemory.Repositories;
+internal sealed class InMemoryUserRepository : IUserRepository
+{
+    public async Task AddAsync(User user)
+    {
+        
+    }
+
+    public Task<User> GetByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/RepForge.Infrastructure/Data/Sqlite/Repositories/SqliteUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using RepForge.Application.Abstractions.Repositories;
+using RepForge.Domain.Users;
+
+namespace RepForge.Infrastructure.Data.Sqlite.Repositories;
+internal class SqliteUserRepository : IUserRepository
+{
+    public Task AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
